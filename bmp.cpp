437d31aec3511ed2e92f64e41d4d551681d28606
s <s>
2017-02-27 16:41:51 +0800

    create class bmp2gif

--- bmp.cpp
+++ bmp.cpp
@@ -4,10 +4,20 @@
 
 #define BMP_PATH		"../bmp/"
 #define	FORMAT_PATH		"%s%d.bmp%c"
-int main(int argc, char* argv[])
+#define FILENAME_GIF	"1.GIF"
+
+class bmp2gif{
+public:
+	inline bool run( char* inPathBmp, char* outFilenameGif );
+
+	gif::GIF* g;
+	bmp2gif(int delay)	{ g = gif::newGIF(delay); }	// nTenMillisecond
+	~bmp2gif()			{ gif::dispose(g);	g = NULL;}
+};
+
+bool bmp2gif::run( char* inPathBmp, char* outFilenameGif )
 {
-	gif::GIF* g = gif::newGIF(3);
-	
+
 	char sPath[20];
 	_snprintf( sPath, sizeof(sPath), FORMAT_PATH, BMP_PATH, 1, '\0');
 	ClImgBMP	bmp;
@@ -26,8 +36,15 @@ int main(int argc, char* argv[])
 		gif::addFrame(g, bmp.bmpInfoHeaderData.biWidth, bmp.bmpInfoHeaderData.biHeight, bmp.imgData, 0);
 	}
 
-	gif::write(g, NULL);
-	gif::dispose(g);
-	g = NULL;
+	gif::write(g, FILENAME_GIF);
+
+	return true;
+}
+
+int main(int argc, char* argv[])
+{
+	bmp2gif	b2g(3);
+	b2g.run( BMP_PATH, FILENAME_GIF );
+
 	return 0;
 }
